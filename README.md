14976813b77aad9e18e0bd5664c75c7d17747cd9
mudassarliaquat <mudasarliaquat@gmail.com>
2024-06-26 11:37:54 +0500

    cli-number-guessing-game

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cli-number-guessing-game
+This is a cli based guessing game
---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6: ",
+    }
+]);
+console.log(answers);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! you guessed right number.");
+}
+else {
+    console.log("You guessed wrong nummber");
+}
